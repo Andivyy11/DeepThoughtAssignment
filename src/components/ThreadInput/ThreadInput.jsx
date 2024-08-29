70ba1655e14e0e90baf52aae41abb86a11f69828
Andivyy11 <andivyy@gmail.com>
2024-08-30 00:34:42 +0530

    add pictures

--- src/components/ThreadInput/ThreadInput.jsx
+++ src/components/ThreadInput/ThreadInput.jsx
@@ -2,7 +2,7 @@ import './ThreadInput.css'
 
 export default function ThreadInput({text , type})
 {
-    return <div className='sub-thread' style={{width: type == 'summary' ? '100%' : '47%'}}>
+    return <div className='sub-thread' style={{width: type === 'summary' ? '100%' : '47%'}}>
     <span>{text}</span>
     <textarea className='sub-thread-input' placeholder='Enter Text Here'>
 
